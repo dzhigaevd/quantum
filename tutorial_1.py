bfc11cafc15b953732d407a28e42361613d7af71
dzhigaevd <ddzhigaev@gmail.com>
2023-08-04 17:52:35 +0200

    final

--- tutorial_1.py
+++ tutorial_1.py
@@ -1,5 +1,5 @@
 from qiskit import *
-from qiskit.tools.visualization import plot_histogram
+from qiskit.tools.visualization import plot_histogram, array_to_latex, plot_bloch_multivector
 import numpy as np
 import matplotlib.pyplot as plt
 from qiskit_aer import Aer
@@ -9,12 +9,18 @@ qr = QuantumRegister(1)
 cr = ClassicalRegister(1)
 
 circuit = QuantumCircuit(qr,cr)
-circuit.h(qr)
-circuit.measure(qr, cr)
+# circuit.h(qr)
+# circuit.measure(qr, cr)
 
 print(cr)
 
-simulator = Aer.get_backend('qasm_simulator')
-result = execute(circuit, backend = simulator, shots=102400).result()
-plot_histogram(result.get_counts())
+# simulator = Aer.get_backend('qasm_simulator')
+# result = execute(circuit, backend = simulator, shots=102400).result()
+# plot_histogram(result.get_counts())
+# plt.show()
+
+simulator = Aer.get_backend('statevector_simulator')
+result = execute(circuit, backend = simulator, shots=1).result()
+state_vector = result.get_statevector()
+plot_bloch_multivector(state_vector)
 plt.show()
